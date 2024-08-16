4d653cd96328da23b7369170dc3cc2a4dfccd25f
FahmiSar <fahmi.sardar@ucalgary.ca>
2024-08-15 22:12:37 -0600

    Managed to get skill upgrades to show properly. Mash's a little weird since she's a unique case but overall its fine

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,29 +4,60 @@ import Tabs from "./Tabs";
 
 function Description({ servant }) {
 	const [activeSkill, setActiveSkill] = useState(null);
+	const [activeExtraSkill, setActiveExtraSkill] = useState(null);
+	const [extraSkills, setExtraSkills] = useState([]);
+
+	const skillOne = servant.skills.filter((skill) => skill.num === 1);
+	const skillTwo = servant.skills.filter((skill) => skill.num === 2);
+	const skillThree = servant.skills.filter((skill) => skill.num === 3);
+
+	const initalSkills = [skillOne[0], skillTwo[0], skillThree[0]];
+
+	const handleExtra = (skill) => {
+		setActiveExtraSkill(skill);
+	}
 
 	const handleClick = (skill) => {
 		setActiveSkill(skill);
+
+		const num = skill.num;
+		let extra = [];
+
+		if (num === 1 && skillOne.length > 1) {
+			extra = skillOne.slice(0);
+		} else if(num === 2 && skillTwo.length > 1){
+			extra = skillTwo.slice(0);
+		} else if(num === 3 && skillThree.length > 1){
+			extra = skillThree.slice(0);
+		}
+
+
+		setExtraSkills(extra);
+		setActiveExtraSkill(extra[0] || null);
 	};
 
-	return (
-		<div>
-			<h1>{servant.name}</h1>
-			<p>
-				Base Atk: {servant.atkBase} | Max Atk {servant.atkMax}
-			</p>
-			<p>
-				Base Hp: {servant.hpBase} | Max Hp: {servant.hpMax}
-			</p>
-			<p>Gender: {servant.gender}</p>
-			<p>Star Absorption: {servant.starAbsorb}</p>
-			<p>Star Generation: {servant.starGen}</p>
-			<p>Instant Death Chance: {servant.instantDeathChance}</p>
+	const skillToDisplay = extraSkills.length > 0 && activeExtraSkill ? activeExtraSkill : activeSkill;
 
-			<h2>Active Skills</h2>
+	return (
+		<div className = "description-box">
+			<div>
+				<h1>{servant.name}</h1>
+				<p>
+					Base Atk: {servant.atkBase} | Max Atk {servant.atkMax}
+				</p>
+				<p>
+					Base Hp: {servant.hpBase} | Max Hp: {servant.hpMax}
+				</p>
+				<p>Gender: {servant.gender}</p>
+				<p>Star Absorption: {servant.starAbsorb}</p>
+				<p>Star Generation: {servant.starGen}</p>
+				<p>Instant Death Chance: {servant.instantDeathChance}</p>
+			</div>
+
 			<div className="skill-details-div">
+			<h2>Active Skills</h2>
 				<Tabs
-					tabs={servant.skills}
+					tabs={initalSkills}
 					activeTab={activeSkill}
 					onClick={handleClick}
 					containerClass="skill-container"
@@ -34,25 +65,39 @@ function Description({ servant }) {
 
 				{activeSkill && typeof activeSkill === "object" && (
 					<div>
-						<table className = "skill-detail-table">
+						{extraSkills.length > 0 && (
+							<Tabs
+								tabs={extraSkills}
+								activeTab={activeExtraSkill}
+								onClick={handleExtra}
+								containerClass="skill-container"
+								showSkillName = {true}
+							/>
+						)}
+
+						<table className="skill-detail-table">
 							<tbody>
 								<tr>
-									<td className = "skill-header">
-                                        <div className = "skill-icon-wrapper">
-                                            <img className = "skill-icon" src = {activeSkill.icon} alt = ""/>
-                                        </div>
-                                        <div className = "skill-name-wrapper">
-                                            <p>{activeSkill.name}</p>
-                                        </div>
-                                    </td>
+									<td className="skill-header">
+										<div className="skill-icon-wrapper">
+											<img
+												className="skill-icon"
+												src={skillToDisplay.icon}
+												alt=""
+											/>
+										</div>
+										<div className="skill-name-wrapper">
+											<p>{skillToDisplay.name}</p>
+										</div>
+									</td>
 								</tr>
-                                <tr>
-                                    <td>
-                                        <div className = "skill-detail-wrapper">
-                                            <p>{activeSkill.detail}</p>
-                                        </div>
-                                    </td>
-                                </tr>
+								<tr>
+									<td>
+										<div className="skill-detail-wrapper">
+											<p>{skillToDisplay.detail}</p>
+										</div>
+									</td>
+								</tr>
 							</tbody>
 						</table>
 					</div>
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 
-function Tabs({ tabs, activeTab, onClick, containerClass}){
+function Tabs({ tabs, activeTab, onClick, containerClass, showSkillName = false}){
     return(
         <div className = {`tabs-container ${containerClass}`}>
             <ul className = "tabs">
                 {tabs.map((tab) => (
-                    <li key = {tab} className = "tab">
+                    <li key = {tab.id} className = "tab">
                         <div className = {`label ${activeTab === tab ? "active" : ""}`}>
                             <a href = {`#${tab}`} onClick = {() => onClick(tab)}>
-                                {typeof tab === "object" ? `Skill ${tab.num}` : tab.replace("_", " ")}
+                            {/**
+                             * the code for this gives me a headache but basically the FGO API organizes skills as an object
+                             * we first check if tab is an object if it isn't then we're working for the class header for the
+                             * homepage so we need the replace stuff
+                             * 
+                             * for skills we want to also check if we want to display skill names if we want to show names
+                             * then tab.name is shown else we display the skill number
+                             */}
+                                {typeof tab === "object" ? 
+                                    showSkillName ? tab.name : `Skill ${tab.num}` 
+                                    : tab.replace("_", " ")}
                             </a>
                         </div>
                     </li>
